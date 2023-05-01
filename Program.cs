af86d4b4f721faa372b20554dabdc5c78a70308f
JocelyndL <60727745+Jocelynardon@users.noreply.github.com>
2023-04-30 21:09:00 -0600

    Contador
    Implementar en dapr.io

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Dapr.Client;
+
+const string storeName = "statestore";
+const string key = "contador";
+
+var daprClient = new DaprClientBuilder().Build();
+var counter = await daprClient.GetStateAsync<int>(storeName, key);
+
+while (true)
+{
+    Console.WriteLine($"Contador = {counter++}");
+
+    await daprClient.SaveStateAsync(storeName, key, counter);
+    await Task.Delay(1000);
+}
